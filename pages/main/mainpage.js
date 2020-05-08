b2c7dfe202711e201d848febb3e8de6bc50077ca
maozhenzhong <dkrose.random@gmail.com>
2020-05-08 23:47:26 +0800

    项目初始化

---
+++ pages/main/mainpage.js
@@ -0,0 +1,79 @@
+// pages/main/mainpage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    scheduleList:[
+      {
+        timeStr:'8:00',
+        content:'Dr Lee call, phone number is 55021796986'
+      },
+      {
+        timeStr:'12:30',
+        content:'Medical care'
+      },
+      {
+        timeStr:'18:00',
+        content:'Night fitness'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
